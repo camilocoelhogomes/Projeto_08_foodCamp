fd0b85ab3fb77b1bba73097ca240a678eeef7755
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-11-29 10:46:36 -0300

    create restaurant sign-in component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import GlobalStyle from './assets/style/GlobalStyle';
 import RoutesConfig from './RoutesConfig/RoutesConfig';
+import UserContext from './context/UserContext';
 
 const App = function () {
+  const [userSign, setUserSign] = useState();
+
+  const updateUserSign = ({ input, value }) => {
+    const newUserSign = { ...userSign };
+    newUserSign[input] = value;
+    setUserSign(newUserSign);
+  };
+
+  const userContext = useMemo(() => ({
+    userSign,
+    updateUserSign,
+  }), [userSign, updateUserSign]);
+
   return (
     <div className="App">
-      <GlobalStyle />
-      <RoutesConfig />
+      <UserContext.Provider value={userContext}>
+        <GlobalStyle />
+        <RoutesConfig />
+      </UserContext.Provider>
     </div>
   );
 };
